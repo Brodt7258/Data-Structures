9fff0ce7cc1d13a1d78a605f1e67dc118e7095f4
Tim Dill <brodt7258@gmail.com>
2020-02-21 05:17:32 -0500

    avl tree, done

--- avl_tree/avl_tree.py
+++ avl_tree/avl_tree.py
@@ -81,7 +81,7 @@ class AVLTree:
     Updates the balance factor on the AVLTree class
     """
     def update_balance(self):
-        l_height, r_height = 0, 0
+        l_height, r_height = -1, -1
 
         if self.node.left: l_height = self.node.left.height
         if self.node.right: r_height = self.node.right.height
@@ -130,28 +130,79 @@ class AVLTree:
     tree is balanced such that the balance factor is
     1 or -1
     """
+    # kind of just hate the premise of this. It makes no sense to assemble the entire tree, and only THEN try to rebalance everything at once
+    # that's the sort of thing that should be handled in small steps on subtrees as values are inserted.
     def rebalance(self):
-        pass
+        self.update_height()
+
+        # a tree cannot be unbalanced without a height of at least 2
+        if self.height < 2:
+            return
+
+        # look down the branches, and handle any imbalances lower on the tree
+        # before trying to fix it at this level
+        if self.node.left:
+            self.node.left.rebalance()
+        
+        if self.node.right:
+            self.node.right.rebalance()
+
+        # note any changes that occured on the downstream rebalance
+        self.update_height_local()
+        self.update_balance()
+
+        # Left hand imbalances
+        if self.balance > 1:
+            ll_sub_height = self.node.left.node.left.height if self.node.left.node.left else -1
+            lr_sub_height = self.node.left.node.right.height if self.node.left.node.right else -1
+
+            # 'Left Left' case
+            if ll_sub_height > lr_sub_height:
+                self.right_rotate()
+            # 'Left Right' case
+            else:
+                self.node.left.left_rotate()
+                self.right_rotate()
+
+        # Right hand imbalances
+        elif self.balance < -1:
+            rl_sub_height = self.node.right.node.left.height if self.node.right.node.left else -1
+            rr_sub_height = self.node.right.node.right.height if self.node.right.node.right else -1
+            # 'Right Right' case
+            if rl_sub_height < rr_sub_height:
+                self.left_rotate()
+            # 'Right Left' case
+            else:
+                self.node.right.right_rotate()
+                self.left_rotate()
         
     """
     Uses the same insertion logic as a binary search tree
     after the value is inserted, we need to check to see
     if we need to rebalance
     """
+    # should probably rewrite this to use a stack, and a new rebalancing method that doesn't make uncessary recursive calls down the tree
     def insert(self, key):
         if key is None:
             return
 
-        elif self.node.key is None:
-            self.node.key = key
+        elif self.node is None:
+            self.node = Node(key)
         
         elif key >= self.node.key:
             if self.node.right is None:
-                self.node.right = AVLTree(key)
+                self.node.right = AVLTree(Node(key))
             else:
                 self.node.right.insert(key)
         else:
             if self.node.left is None:
-                self.node.left = AVLTree(key)
+                self.node.left = AVLTree(Node(key))
             else:
                 self.node.left.insert(key)
+        
+
+        self.update_height_local()
+        self.update_balance()
+
+        if self.balance > 1 or self.balance < -1:
+            self.rebalance()
